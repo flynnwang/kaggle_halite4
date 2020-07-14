48b53d6d788d4ca5b744c45ae125112430a8db37
Fei Wang <flynnwang@didiglobal.com>
2020-07-14 14:00:01 +0800

    enable attack in agent_bee_v2.2.4.2

--- agent_bee_v2.2.4.2.py
+++ agent_bee_v2.2.4.2.py
@@ -1,23 +1,6 @@
 #!/usr/bin/env python
 """
-halite_per_turn:
-* Use current halite value when no ship on it
-* -1000 if enemy with with less halite
-
--=-=-=-=-=-=-=-=-=-=-=-|  Your Halite 4 Trueskill Ladder |-=-=-=-=-=-=-=-=-=-=-=-
-
-Tournament - ID: t8uPQv, Name: Your Halite 4 Trueskill Ladder | Dimension - ID: nU3Byg, Name: Halite 4 Dimension
-Status: running | Competitors: 7 | Rank System: trueskill
-
-Total Matches: 572 | Matches Queued: 58
-Name                           | ID             | Score=(μ - 3σ)  | Mu: μ, Sigma: σ    | Matches
-bee v1.8                       | 61fgpgTPMl9V   | 29.9049534      | μ=32.032, σ=0.709  | 263
-bee v2.2.4 attack              | f1NKoInHuloY   | 28.2238038      | μ=30.301, σ=0.692  | 304
-bee v2.2.4.1                   | pR5eEG6XAfKY   | 28.0136128      | μ=30.074, σ=0.687  | 279
-optimus_mining                 | xdAojSbKsiWj   | 25.9457208      | μ=27.991, σ=0.682  | 331
-c40                            | 5ZsvJC3K6CNi   | 24.7707335      | μ=26.817, σ=0.682  | 357
-v3.3 no min                    | 4OlkXQDdwsqE   | 21.7383512      | μ=23.818, σ=0.693  | 371
-swarm                          | psFwrYs71HB7   | 16.2060266      | μ=18.518, σ=0.771  | 383
+* Enable attack strategy.
 """
 
 import copy
@@ -573,9 +556,9 @@ class ShipStrategy:
           ship_budget -= 1
 
       # Skip attack enemy if not having enough ships.
-      # if (self.step < BEGINNING_PHRASE_END_STEP and
-      # self.step > NEAR_ENDING_PHRASE_STEP):
-      continue
+      if (self.step < BEGINNING_PHRASE_END_STEP and
+          self.step > NEAR_ENDING_PHRASE_STEP):
+        continue
 
       # send destory ship from inner.
       if (ship_budget > 0 and
