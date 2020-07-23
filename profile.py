b1bceec539e40e8ab25a9cd0f0fd536048151658
Fei Wang <wang.flynn@gmail.com>
2020-07-24 06:07:03 +0800

    update profile

--- profile.py
+++ profile.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import json
 import os
 
 from pycallgraph import PyCallGraph
 from pycallgraph import Config
 from pycallgraph.output import GraphvizOutput
+
+import agent_bee_v4_0_4_1 as agent
 
 work_dir = "/ssd/wangfei/repo/flynn/kaggle_halite4/"
 replay_path = os.path.join(work_dir, "debug_replay", "1746731.json")
@@ -14,15 +17,15 @@ def agent(name, collection=False):
   return os.path.join(work_dir, collection and 'collection' or '', name)
 
 
-def run():
-  Replayer()
-  env = make("halite", {'episodeSteps': 100}, debug=True)
-  a = agent("agent_bee_v2.1.1.py")
-  env.run([a, "random", "random", "random"])
-
-
 config = Config(max_depth=10)
 graphviz = GraphvizOutput(output_file='filter_max_depth.png')
 
+with open(replay_path, 'r') as f:
+  replay_json = json.loads(f.read())
+
+strategy = agent.ShipStrategy()
+replayer = Replayer(strategy, replay_json, player_id=1)
+replayer.play(250)
+
 with PyCallGraph(output=graphviz, config=config):
-  run()
+  replayer.play(100)
