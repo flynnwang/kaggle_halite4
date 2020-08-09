a13278c9b25ce59a9fa4b6ebe82bbaafc58a1095
Fei Wang <flynnwang@didiglobal.com>
2020-08-09 19:20:19 +0800

    use v1 coordinator

--- a/matrix/coodinator.py
+++ a/matrix/coodinator.py
@@ -7,25 +7,29 @@ import argparse
 
 import uuid
 
-def run(episode_dir, model_dir, batch):
 
-  # epsilon = 1.0
-  epsilon = 0.05
-  epsilon_decay = 0.995
+EPSILON_DECAY = 0.995
+
+
+def run(episode_dir, model_dir, batch, epsilon, episode_steps, batch_size):
 
   for b in range(batch):
     time_tag = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     batch_dir = os.path.join(episode_dir, time_tag)
 
-    subprocess.check_call(["python3", "lsd.py", "-o", batch_dir, '-m', model_dir,
-                           '--epsilon', str(epsilon)])
+    subprocess.check_call(["python3", "lsd.py", "-o", batch_dir,
+                           '-m', model_dir,
+                           '--epsilon', str(epsilon),
+                           '--episode_steps', str(episode_steps),
+                           '--batch_size', str(batch_size)])
     print("All episodes generation finished: ", batch_dir)
 
-    subprocess.check_call(["python3", "batch_train.py", "-e", batch_dir, '-m', model_dir])
+    subprocess.check_call(["python3", "batch_train.py", "-e", batch_dir,
+                           '-m', model_dir])
     print("One training batch finished: ", batch_dir)
     print("Step: %s, epsilon value: %s" % (b, epsilon))
 
-    epsilon *= epsilon_decay
+    epsilon *= EPSILON_DECAY
 
 
 
@@ -34,8 +38,13 @@ def main():
   parser.add_argument('-e', '--episode_dir', required=True)
   parser.add_argument('-m', '--model_dir', required=True)
   parser.add_argument('-b', '--batch', required=True, type=int)
+  parser.add_argument('--epsilon', type=float, default=1.0)
+  parser.add_argument('--episode_steps', type=int, default=200)
+  parser.add_argument('--batch_size', type=int, default=24)
+
   args = parser.parse_args()
-  run(args.episode_dir, args.model_dir, args.batch)
+  run(args.episode_dir, args.model_dir, args.batch,
+      args.epsilon, args.episode_steps, args.batch_size)
 
 
 if __name__ == "__main__":
